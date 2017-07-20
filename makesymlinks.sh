d76f6d60e2ccb09bd63daa3cad03a53f4af3e1ba
Jonathan Armond <jon.armond@gmail.com>
2017-07-20 19:29:49 +0100

    Process multiple directories

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -1,23 +1,19 @@
 #!/bin/bash
 
-# Config file directory
-confdir=~/conf
 targetdir=~
 
-# List of files/dirs to symlink with a dot prefix
-dotfiles=`ls -d $confdir/dot*`
-for i in $dotfiles
+# Config file directories
+declare -a confdir=("$HOME/conf" "$HOME/conf/private")
+for d in "${confdir[@]}"
 do
-    f=`basename $i`
-    fdot=${f:3}
-    echo $fdot
-    echo $f
-    ln -fsnv $confdir/$f $targetdir/.$fdot
+    echo "Processing directory $d"
+    # List of files/dirs to symlink with a dot prefix
+    dotfiles=`ls -d $d/dot*`
+    for i in $dotfiles
+    do
+        f=`basename $i`
+        fdot=${f:3}
+        ln -fsnv $d/$f $targetdir/.$fdot
+    done
 done
 
-# List of files/dirs to symlink without name changes
-#trees=""
-#for i in $trees
-#do
-#    ln -Ffsv $confdir/$i $targetdir/$i
-#done
